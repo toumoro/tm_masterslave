f7ccd2d5dfbf0156bf90a03385920b1a29d65b38
Simon Ouellet <simon.ouellet@toumoro.com>
2020-05-13 09:08:32 -0400

    testing with 10.4.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '9.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
